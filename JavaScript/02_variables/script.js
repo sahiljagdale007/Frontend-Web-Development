8b5ae29a37b41d81998f17fd038eed5f504e83e8
Sahil <sahilashokjagdale@gmail.com>
2025-04-19 14:33:10 +0530

    init commit

---
+++ JavaScript/02_variables/script.js
@@ -0,0 +1,15 @@
+let username = 'sahil'
+let userIntro = 'My name is Sahil'
+let age = 25
+let isHappy = true
+
+let call = 'Hi myself' + ' ' + username + ' ' + age
+
+let firstname = 'sahil'
+let lastname = 'jagdale'
+
+let intro = M='My name is '+ firstname +' '+ lastname + '.'
+
+let a   //undefined
+
+let b = null //object
